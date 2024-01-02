22ee800dedd1861b2ee291c411382e68127c9736
UtsavMandal2022 <utsav2016mandal@gmail.com>
2024-01-03 01:37:54 +0530

    some changes

--- User/views.py
+++ User/views.py
@@ -4,7 +4,7 @@ from rest_framework.decorators import api_view, permission_classes, authenticati
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from moviebackend.auth import *
-# from newsbackend.permissions import *
+# from moviebackend.permissions import *
 from django.contrib.auth.models import User
 from .models import *
 import random
--- moviebackend/asgi.py
+++ moviebackend/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for newsbackend project.
+ASGI config for moviebackend project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
